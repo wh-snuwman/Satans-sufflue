f62e9ebbb5de3bfea0d11b759128968bfcf7d647
snuwman <snowman34712@gmail.com>
2025-12-01 23:16:57 +0900

    UI 이미지 추가

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 import {PHI} from "/@phi/src/script/PHI.js"
-// import '/socekt.js'
+import {online} from '/socekt.js'
 
 
 let mousePos = [0,0];
@@ -17,9 +17,7 @@ let click = false;
     const ctx = textCanvas.getContext('2d')
     ctx.font = "20px Arial"; // 글자 크기 + 폰트
     ctx.fillStyle = "blue"; // 글자 색
-    
     phi.display([innerWidth,innerHeight]);
-    
     const deck = {
         BACK:await phi.imgLoad('/src/img/deck/BACK.png'),
         TEST:await phi.imgLoad('/src/img/deck/TEST.png'),
@@ -87,157 +85,306 @@ let click = false;
 
     }
 
-    let rank = [];
+    const skinImg = [
+        await phi.imgLoad('/src/img/skin/0.png'),
+        await phi.imgLoad('/src/img/skin/1.png'),
+        await phi.imgLoad('/src/img/skin/2.png'),
+    ]
+
+    const skin = {
+        'test0':{
+            nomarl:phi.object(skinImg[0],[180,230],null),
+            speak:phi.object(skinImg[0],[0,0],null),
+        },
+        'test1':{
+            nomarl:phi.object(skinImg[1],[870,130],null),
+            speak:phi.object(skinImg[1],[0,0],null),
+        },
+        'test2':{
+            nomarl:phi.object(skinImg[2],[1530,230],null),
+            speak:phi.object(skinImg[2],[0,0],null),
+        },
+    }
+
+    const uiImg = {
+        back_box : await phi.imgLoad('/src/img/ui/back_box.png'),
+        char_box : await phi.imgLoad('/src/img/ui/char_box.png'),
+        long_bar: await phi.imgLoad('/src/img/ui/long_bar.png'),
+        main_menu_btn : await phi.imgLoad('/src/img/ui/main_menu_btn.png'),
+        make_room_btn : await phi.imgLoad('/src/img/ui/make_room_btn.png'),
+        join_btn : await phi.imgLoad('/src/img/ui/join_btn.png'),
+        rect : await phi.imgLoad('/src/img/ui/rect.png'),
+        setting_btn : await phi.imgLoad('/src/img/ui/setting_btn.png'),
+        short_bar : await phi.imgLoad('/src/img/ui/short_bar.png'),
+    }
+
+    const gameMenuUI = {
+        backBoxObj : phi.object(uiImg.back_box,[(innerWidth-uiImg.back_box.width)/2,(innerHeight-uiImg.back_box.height)/2],null),
+        long_bar : phi.object(uiImg.long_bar,[(innerWidth-uiImg.long_bar.width)/2,300],null),
+        join_btn : phi.object(uiImg.join_btn,[(innerWidth-uiImg.join_btn.width)/2 - 405,400],null),
+        make_room_btn : phi.object(uiImg.make_room_btn,[(innerWidth-uiImg.make_room_btn.width)/2 - 405,500],null),
+        setting_btn : phi.object(uiImg.setting_btn,[(innerWidth-uiImg.setting_btn.width)/2 - 405,600],null),
+        main_menu_btn : phi.object(uiImg.main_menu_btn,[(innerWidth-uiImg.main_menu_btn.width)/2 - 405,700],null),
+        char_box : phi.object(uiImg.char_box,[(innerWidth-uiImg.char_box.width)/2 - 80,(innerHeight-uiImg.char_box.height)/2 + 45],null),
+        
+        
+        
+        user_profile_0 : phi.object(uiImg.short_bar,[(innerWidth-uiImg.short_bar.width)/2 + 365,400],null),
+        user_profile_1 : phi.object(uiImg.short_bar,[(innerWidth-uiImg.short_bar.width)/2 + 365,500],null),
+        user_profile_2 : phi.object(uiImg.short_bar,[(innerWidth-uiImg.short_bar.width)/2 + 365,600],null),
+        user_profile_3 : phi.object(uiImg.short_bar,[(innerWidth-uiImg.short_bar.width)/2 + 365,700],null),
+        
+        user_picture_0 : phi.object(uiImg.rect,[(innerWidth-uiImg.rect.width)/2 + 110,400],null),
+        user_picture_1 : phi.object(uiImg.rect,[(innerWidth-uiImg.rect.width)/2 + 110,500],null),
+        user_picture_2 : phi.object(uiImg.rect,[(innerWidth-uiImg.rect.width)/2 + 110,600],null),
+        user_picture_3 : phi.object(uiImg.rect,[(innerWidth-uiImg.rect.width)/2 + 110,700],null),
+
+    
+    }
+
+
+    window.rank = [];
     const res = await fetch("rank.json")
     const data = await res.json()
-    rank = [...data.rank]
+    window.rank = [...data.rank]
     const deckSizeRatio = 0.5
     const cardSize = [deck.BACK.width*deckSizeRatio,deck.BACK.height*deckSizeRatio]
-    const cardsInf = []
+    window.cardsInf = []
     for (let i = 0; i<52; i++){
         cardsInf.push({
             obj : phi.object(deck.TEST,[(innerWidth - cardSize[0])/2,(innerHeight - cardSize[1])/2],cardSize),
-            aprObj : phi.object(deck[rank[i]],[(innerWidth - cardSize[0])/2,(innerHeight - cardSize[1])/2],cardSize),
+            aprObj : phi.object(deck[window.rank[i]],[(innerWidth - cardSize[0])/2,(innerHeight - cardSize[1])/2],cardSize),
             isSelect: false,
             posFixFlag:false,
             pos1:[0,0],
             pos2:[0,0],
-            rank:rank[i],
+            rank:window.rank[i],
             show:true,
             owner:null,
             preClick:false,
         })
     }
-    let playersDeck = {
-        'p0':['SA','SK','SQ','SJ','S10','S9'],
-        'p1':['DA','DK','DQ','DJ','D10','D9'],
-        'p2':['HA','HK','HQ','HJ','H10'],
-        'p3':['CA','CK','CQ','CJ','C10'],
-        // 중복카드가 있으면 오류남. 주의! 
+
+    window.players = {
+        'p0':{
+            // nickname:'김대호',
+            // profile:0,
+            // description:'hello wordl!',
+            // skin:null,
+            // level:0,
+            // rank:0,
+        },
+        'p1':{
+            // nickname:'김동호',
+            // profile:0,
+            // description:'세계정복 같이하실분 모집합니다',
+            // skin:'test0',
+            // level:0,
+            // rank:0,
+        },
+        'p2':{
+            // nickname:'익명의누군가',
+            // profile:0,
+            // description:"안녕하세요",
+            // skin:'test1',
+            // level:0,
+            // rank:0,
+        },
+        'p3':{
+            // nickname:'PANCAKE',
+            // profile:0,
+            // description:"집가고싶다ㅠㅠ",
+            // skin:'test2',
+            // level:0,
+            // rank:0,
+        },
     }
 
+    // let playersDeck = {
+    //     'p0':['SA','SK','SQ','SJ','S10','S9'],
+    //     'p1':['DA','DK','DQ','DJ','D10','D9'],
+    //     'p2':['HA','HK','HQ','HJ','H10'],
+    //     'p3':['CA','CK','CQ','CJ','C10'],
+    //     // 중복카드가 있으면 오류남. 주의! 
+    // }
+    let playersDeck = {
+        'p0':[],
+        'p1':[],
+        'p2':[],
+        'p3':[],
+    }
     let posList = {
         'p0':[innerWidth/2,innerHeight-((cardSize[1]/2)*3)],
         'p1':[300,500],
-        'p2':[innerWidth/2,300],
+        'p2':[innerWidth/2,400],
         'p3':[innerWidth-300,500],
     }
-    const centerDeckPos = [(innerWidth - cardSize[0])/2 - 100,(innerHeight - cardSize[1])/2 + 100]
+    const centerDeckPos = [(innerWidth - cardSize[0])/2 - 75,(innerHeight - cardSize[1])/2 + 150]
     const ver_line = phi.object(deck.TEST,[innerWidth/2,0],[1,innerHeight])
     function Text(text,pos){
         ctx.fillText(text, pos[0],pos[1]);
     }
-
     let selectCard = null; 
-    let lastCard = 'C2'
-
-    
+    let lastCard = ''
     window.scrollTo({
         top:1512,
         behavior:'smooth'
     });
 
+
+    window.nickname = 'Anonymous' + phi.random(-10000000,10000000);
+    window.password = '';
+    window.profile = 0
+    window.description = '이게보인다면 에러다'
+    window.skin = 'test0'
+    window.level = 0
+    window.rank = 0
+
+    
+    let resetFixPos = false
+    function addCard(player,card){
+        resetFixPos = true
+        playersDeck[player].push(card)
+    }
+
+    online(); //온라인접속 시작
+    // addCard('p0','C3')
+
+    window.scene = 'menu-game';
+
+
     phi.mainLoop(() => {
         ctx.clearRect(0, 0, textCanvas.width, textCanvas.height);
         phi.fill(0.1,0.1,0.1,1)
-        
         phi.blit(ver_line)
-        
-        
 
-        for (let inf of cardsInf){
-            let obj = inf.obj
-            let apr_obj = inf.aprObj
-            let rank = inf.rank
-            let owner = inf.owner
-            let show = inf.show
-
-            for (let pName in playersDeck){
-                const myDeck = playersDeck[pName]
-                if (myDeck.includes(rank)){
-                    const cardNumber = myDeck.indexOf(rank)
-
+        if (window.scene == 'ingmae-onecard'){
+            for(let pName in window.players){
+                let playerInf = window.players[pName]
+                const nickname = playerInf.nickname
+                const profile = playerInf.profile
+                const description = playerInf.description
+                const playerSkin = playerInf.skin
+                const level = playerInf.level
+                const playerRank = playerInf.rank
+                
+                if (nickname){
                     if (pName !== 'p0'){
-                        phi.Goto(obj,
-                            [(posList[pName][0] - (myDeck.length * cardSize[0])/3.25) + cardNumber * cardSize[0]/2, 
-                            posList[pName][1]]
-                        )
-                        phi.rotate(obj,20- obj.angle)
-                        
-                    } else {
-                        phi.Goto(obj,
-                            [(posList[pName][0] - (myDeck.length * cardSize[0])/2) + cardNumber * cardSize[0], 
-                            posList[pName][1]]
-                        )
-                        if (!inf.posFixFlag){
-                            inf.pos1 = [obj.x,obj.y]
-                            inf.posFixFlag = true
+                        phi.blit(skin[playerSkin].nomarl)
+                    }
+                }
+                
+            }
+            for (let inf of window.cardsInf){
+                let obj = inf.obj
+                let apr_obj = inf.aprObj
+                let rank = inf.rank
+                let owner = inf.owner
+                let show = inf.show
+    
+                // console.log(playersDeck['p0'])
+    
+                for (let pName in playersDeck){
+                    const myDeck = playersDeck[pName]
+                    
+                    if (myDeck.includes(rank)){
+                        const cardNumber = myDeck.indexOf(rank)
+    
+                        if (pName !== 'p0'){
+                            phi.Goto(obj,
+                                [(posList[pName][0] - (myDeck.length * cardSize[0])/3.25) + cardNumber * cardSize[0]/2, 
+                                posList[pName][1]]
+                            )
+                            phi.rotate(obj,20- obj.angle)
+                            
+                        } else {
+                            // console.log(myDeck)
+    
+                            phi.Goto(obj,
+                                [(posList[pName][0] - (myDeck.length * cardSize[0])/2)+ cardNumber * cardSize[0], 
+                                posList[pName][1]]
+                            )
+                            phi.rotate(obj,10- obj.angle)
+    
+    
+    
+                            if (!inf.posFixFlag){
+                                inf.pos1 = [obj.x,obj.y]
+                                inf.posFixFlag = true
+                            }
+    
+                            if (resetFixPos){
+                                inf.posFixFlag = false
+                            }
+                        }
+    
+                        owner = pName
+                    }
+    
+                    if (owner == 'p0' && (phi.isEncounterPos(obj,mousePos) && click)){
+                        inf.isSelect  = !inf.isSelect
+                        if (inf.isSelect){
+                            selectCard = rank
                         }
                     }
-
-                    owner = pName
-                }
-
-                if (owner == 'p0' && (phi.isEncounterPos(obj,mousePos) && click)){
-                    inf.isSelect  = !inf.isSelect
-                    if (inf.isSelect){
-                        selectCard = rank
+    
+                    if (owner == 'p0'){
+                        if (selectCard == rank){
+                            phi.Goto(obj,[inf.pos1[0],inf.pos1[1]-40])
+                        } else {
+                            phi.Goto(obj,[inf.pos1[0],inf.pos1[1]])
+                            
+                        }
                     }
-                }
-                
-                console.log(selectCard)
-
-
-                if (owner == 'p0'){
-                    if (selectCard == rank){
-                        phi.Goto(obj,[inf.pos1[0],inf.pos1[1]-40])
+    
+                    
+                    if (owner !== 'p0'){
+                        show = false
                     } else {
-                        phi.Goto(obj,[inf.pos1[0],inf.pos1[1]])
-                        
-                    }
-                }
-
-                
-                if (owner !== 'p0'){
-                    show = false
-                } else {
-                    show = true
-                }
-
-                if (owner == null){
-                    if (lastCard == rank){
-                        phi.Goto(obj,
-                            [centerDeckPos[0]+cardSize[0]*1.5,centerDeckPos[1]]
-                        )
                         show = true
-                    } else {
-                        phi.Goto(obj,
-                            centerDeckPos
-                        )
                     }
+    
+                    if (owner == null){
+                        if (lastCard == rank){
+                            phi.Goto(obj,
+                                [centerDeckPos[0]+cardSize[0]*1.5,centerDeckPos[1]]
+                            )
+                            show = true
+                        } else {
+                            phi.Goto(obj,
+                                centerDeckPos
+                            )
+                        }
+                    }
+    
+                    
                 }
-
-                
+    
+                phi.moveX(apr_obj,((obj.x) - apr_obj.x) / 7)
+                phi.moveY(apr_obj,((obj.y) - apr_obj.y) / 7)
+                phi.rotate(apr_obj,((obj.angle) - apr_obj.angle) / 7)
+                Text(rank+' '+owner,[obj.x,obj.y+30])
+                if (show){
+                    phi.blit(apr_obj)
+                } else {
+                    const obj_ = {...apr_obj}
+                    obj_.img = deck.BACK
+                    phi.blit(obj_)
+                }
+                // phi.blit(obj)
+    
             }
-
-
-
-            phi.moveX(apr_obj,((obj.x) - apr_obj.x) / 7)
-            phi.moveY(apr_obj,((obj.y) - apr_obj.y) / 7)
-            phi.rotate(apr_obj,((obj.angle) - apr_obj.angle) / 7)
-            Text(rank+' '+owner,[obj.x,obj.y+30])
-            if (show){
-                phi.blit(apr_obj)
-            } else {
-                const obj_ = {...apr_obj}
-                obj_.img = deck.BACK
-                phi.blit(obj_)
+        } else if (window.scene == 'menu-game'){
+            for(let name in gameMenuUI){
+                const ui = gameMenuUI[name]
+                phi.blit(ui)
             }
-            // phi.blit(obj)
-
         }
 
+
+
         if(click){click=false;}
+        if(resetFixPos){resetFixPos=false;}
 
 
     });
--- socekt.js
+++ socekt.js
@@ -2,28 +2,39 @@
 
 
 
-window.sc = new WebSocket('ws://localhost:3000')
+export function online(){
+    window.sc = new WebSocket('ws://localhost:3000')
+    const nickname = window.nickname
+    const password = window.password
+    
+    sc.onopen = () => {
+        console.log('✅ 서버에 연결 되었습니다!')
+    }
+    
+    
+    sc.onmessage = (event) => {
+        let msg = JSON.parse(event.data);
 
-
-sc.onopen = () => {
-    console.log('✅ 서버에 연결 되었습니다!')
+        if (msg.code == '0.1.1') {
+            console.log(`✅ 로그인성공: ${nickname}`)
+        }
+    }
+     
+    sc.onerror = (err) => {
+        console.log('‼️ 에러발생:'+ err)
+    }
+    
+    sc.onclose = () => {
+        null
+    }
+    
+    sc.addEventListener('open', () => {
+        sc.send(JSON.stringify({ 
+            nick: nickname,
+            code:"0.1",
+            pw: password, 
+            anonymous:true 
+        }));
+    });
 }
-
-
-sc.onmessage = (event) => {
-    const msg = event.data
-}
-
-sc.onerror = (err) => {
-    console.log('‼️ 에러발생:'+ err)
-}
-
-sc.onclose = () => {
-    null
-}
-
-
-
-
-
 
